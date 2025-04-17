d985c8b9dcf0e0c8baa30a68a7972a9d43b185de
Robin Bezák <robin.bezak.99@gmail.com>
2025-04-17 13:16:00 +0200

    added three column grid to the footer with author of the website and the city country-code, time

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Box } from "@mui/material";
+import React, {useEffect, useState} from 'react';
+import {Box, Typography} from "@mui/material";
 import Marquee from "react-fast-marquee";
 import { Divider } from "./Divider.tsx";
 import { ParallaxText } from "./ParallaxText.tsx";
@@ -7,6 +7,30 @@ import { Email } from "./Email.tsx";
 
 
 export const Footer: React.FC = () => {
+    const [hours, setHours] = useState<string>('');
+    const [minutes, setMinutes] = useState<string>('');
+    const [showColon, setShowColon] = useState<boolean>(true);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const now = new Date();
+            const timeString = now.toLocaleTimeString('cs-CZ', {
+                timeZone: 'Europe/Prague',
+                hour: '2-digit',
+                minute: '2-digit'
+            });
+            const [currentHours, currentMinutes] = timeString.split(':');
+
+            setHours(currentHours);
+            setMinutes(currentMinutes);
+            setShowColon(prevShowColon => !prevShowColon);
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
     return (
         <Box>
             <Divider width="100%" thickness={2} marginY={2} animateFrom="left" />
@@ -22,6 +46,75 @@ export const Footer: React.FC = () => {
             <Divider width="100%" thickness={2} marginY={2} animateFrom="right" />
 
             <Email email="robin.bezak.99@gmail.com" />
+
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: { xs: 'column', sm: 'row' },
+                    justifyContent: 'space-between',
+                    alignItems: { xs: 'center', sm: 'flex-start' },
+                    flexWrap: 'wrap',
+                    px: 5,
+                    mt: 4,
+                    gap: 2,
+                    pb: 8,
+                    pt: 4,
+                }}
+            >
+                <Box sx={{
+                    width: { xs: '100%', sm: 'auto' },
+                    minWidth: { sm: '25%' },
+                    textAlign: { xs: 'center', sm: 'left' },
+                    textTransform: 'uppercase',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    gap: 2,
+                }}>
+                    <Typography
+                        variant="body2"
+                        sx={{
+                            fontSize: '16px',
+                            whiteSpace: 'nowrap'
+                        }}
+                    >
+                        Prague, CZ
+                    </Typography>
+                    <Typography
+                        variant="body2"
+                        sx={{
+                            fontSize: '16px',
+                            fontFamily: "'Poppins SemiBold', sans-serif",
+                        }}
+                    >
+                        {hours}{showColon ? ':' : ' '}{minutes}
+                    </Typography>
+                </Box>
+
+                <Box sx={{
+                    width: { xs: '100%', sm: 'auto' },
+                    minWidth: { sm: '25%' },
+                    textAlign: 'center',
+                    fontFamily: "'Poppins Regular', sans-serif",
+                }}>
+                    SOCIAL BUTTONS MAPPING
+                </Box>
+
+                <Box sx={{
+                    width: { xs: '100%', sm: 'auto' },
+                    minWidth: { sm: '25%' },
+                    textAlign: { xs: 'center', sm: 'right' },
+                    textTransform: 'normal',
+                }}>
+                    <Typography
+                        variant="body2"
+                        sx={{
+                            fontSize: '16px',
+                        }}
+                    >
+                        <span style={{ fontWeight: 800 }}>Coded by</span> @Robin Bezak
+                    </Typography>
+                </Box>
+            </Box>
 
         </Box>
     );
